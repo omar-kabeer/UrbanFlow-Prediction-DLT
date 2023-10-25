7a317c342dc906de0d6a37c1621202cbd5d44bad
Umar Kabir <61500640+omar-kabeer@users.noreply.github.com>
2023-10-25 19:19:10 +0100

    adding kurtosis and skew function

--- src/functions.py
+++ src/functions.py
@@ -16,3 +16,24 @@ def combine_dataframes(dfs):
     for df in dfs:
         combined_df = combined_df.append(df, ignore_index=True)  # append the rows of the current dataframe to the previous one
     return combined_df
+
+def skew_kurt(data, col):
+    """This code defines a function named skew_kurt that calculates the skewness and kurtosis of a given column in a dataset.
+    It also creates a histogram of the column with annotations for the mean, median, and mode.
+    """
+    # Calculate skewness and kurtosis of Income column
+    _skewness = skew(data[col])
+    _kurtosis = kurtosis(data[col])
+
+    # Create histogram of Income column with mean, median, and mode
+    sns.histplot(data=data, x=col, kde=True)
+    plt.axvline(data[col].mean(), color='r', linestyle='--', label='Mean')
+    plt.axvline(data[col].median(), color='g', linestyle='--', label='Median')
+    plt.axvline(data[col].mode()[0], color='b', linestyle='--', label='Mode')
+    plt.legend()
+
+    # Add text annotation for skewness and kurtosis values
+    plt.annotate('Skewness: {:.2f}'.format(_skewness), xy=(0.5, 0.9), xycoords='axes fraction')
+    plt.annotate('Kurtosis: {:.2f}'.format(_kurtosis), xy=(0.5, 0.85), xycoords='axes fraction')
+
+    plt.show()
